e2e869327b7f7956a1e0da4c1e8a50e22282a280
Michi-123 <hairstyle2004@yahoo.co.jp>
2025-08-31 16:44:46 +0900

    Fix init

--- util/functions/__init__.py
+++ util/functions/__init__.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('./')
 
+from .get_aozora_corpus import get_aozora_corpus
 from .show_parameters import show_parameters
 from .log_run import log_run
 from .show_loss_graph import show_loss_graph
