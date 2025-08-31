063a9278aeafe3ba6d7c2757aa711f095ef4d48d
Michi-123 <hairstyle2004@yahoo.co.jp>
2025-08-31 16:05:16 +0900

    init bug fix

--- util/functions/__init__.py
+++ util/functions/__init__.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('./')
 
-from show_parameters import show_parameters
+from .show_parameters import show_parameters
 from .log_run import log_run
 from .show_loss_graph import show_loss_graph
