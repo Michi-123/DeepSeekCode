8215bda8b61e055d69e29c313be043d4bbbfd933
michi123 <you@example.com>
2026-05-23 20:55:17 +0900

    removed test code from deepseekcode

--- __init__.py
+++ __init__.py
@@ -1,9 +1,7 @@
 import sys
 sys.path.append('./')
 
-from DeepSeekCode.src.KVCache.KVCache import KVCache
-from DeepSeekCode.src.RoPE.RoPE import precompute_freqs_cis
-from DeepSeekCode.src.RoPE.RoPE import apply_rope
+from DeepSeekCode.src import deepseekcode
 
 from DeepSeekCode.src import MoE
 from DeepSeekCode.src import MLA
@@ -12,6 +10,11 @@ from DeepSeekCode.src import KVCache as KVC
 from DeepSeekCode.src import MTP
 from DeepSeekCode.src import Transformer
 from DeepSeekCode.src import DeepSeek
+
+from DeepSeekCode.src.KVCache.KVCache import KVCache
+
+from DeepSeekCode.src.RoPE.RoPE import precompute_freqs_cis
+from DeepSeekCode.src.RoPE.RoPE import apply_rope
 
 from DeepSeekCode.src.DeepSeek import create_causal_mask
 from DeepSeekCode.src.DeepSeek import RMSNorm
