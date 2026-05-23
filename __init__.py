e8af6aaa7fd34ec2acb253a97428fe21044ef604
michi123 <you@example.com>
2026-05-23 21:54:13 +0900

    edit

--- __init__.py
+++ __init__.py
@@ -2,10 +2,32 @@ import sys
 sys.path.append('./')
 
 from DeepSeekCode.src import deepseekcode
-from DeepSeekCode.src.KVCache.KVCache import KVCache
-from DeepSeekCode.src.RoPE.RoPE import precompute_freqs_cis
-from DeepSeekCode.src.RoPE.RoPE import apply_rope
+from DeepSeekCode.src.deepseekcode import MLA
+from DeepSeekCode.src.deepseekcode import MHA
+from DeepSeekCode.src.deepseekcode import precompute_freqs_cis
+from DeepSeekCode.src.deepseekcode import apply_rope
+from DeepSeekCode.src.deepseekcode import RoPE
+from DeepSeekCode.src.deepseekcode import KVCache
+from DeepSeekCode.src.deepseekcode import Expert
+from DeepSeekCode.src.deepseekcode import MoE
+from DeepSeekCode.src.deepseekcode import MainModel
+from DeepSeekCode.src.deepseekcode import MTPModule
+from DeepSeekCode.src.deepseekcode import TransformerBlock
 
+from DeepSeekCode.src.deepseekcode import DeepSeekCode
+from DeepSeekCode.src.deepseekcode import FeedForward
+from DeepSeekCode.src.deepseekcode import LayerNorm
+from DeepSeekCode.src.deepseekcode import RMSNorm
+
+from DeepSeekCode.src.deepseekcode import Args
+from DeepSeekCode.src.deepseekcode import create_causal_mask
+from DeepSeekCode.src.deepseekcode import create_padding_mask
+from DeepSeekCode.src.deepseekcode import PositionalEncoding
+from DeepSeekCode.src.deepseekcode import pad_mask_after_eos
+
+
+
+"""
 from DeepSeekCode.src import MoE
 from DeepSeekCode.src import MLA
 from DeepSeekCode.src import RoPE
@@ -14,5 +36,11 @@ from DeepSeekCode.src import MTP
 from DeepSeekCode.src import Transformer
 from DeepSeekCode.src import DeepSeek
 
+from DeepSeekCode.src.KVCache.KVCache import KVCache
+
+from DeepSeekCode.src.RoPE.RoPE import precompute_freqs_cis
+from DeepSeekCode.src.RoPE.RoPE import apply_rope
+
 from DeepSeekCode.src.DeepSeek import create_causal_mask
 from DeepSeekCode.src.DeepSeek import RMSNorm
+"""
